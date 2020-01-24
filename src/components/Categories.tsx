2ad1cea184f5f03628855e9331a846d3a08bd3ba
Cem Özden <cemozden.tr@gmail.com>
2020-01-24 16:25:23 +0100

    Fixing tslint issue

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { FeedConfigManager, FeedCategory, FeedConfig } from "../config/FeedConfigManager";
 import { ContextMenuTrigger, ContextMenu, MenuItem } from "react-contextmenu";
 
