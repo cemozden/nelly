3d7f49a23a2232356fbaba8a5e25067f31d3954b
Cem Özden <cemozden.tr@gmail.com>
2020-01-28 15:10:27 +0100

    Added categories text to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import CategoryHeader from "./CategoryHeader";
 import Categories from "./Categories";
 import { FeedConfigManager } from "../config/FeedConfigManager";
+import { ApplicationContext } from "../App";
 
 export interface SideBarProps {
     feedConfigManager : FeedConfigManager
@@ -9,12 +10,16 @@ export interface SideBarProps {
 
 const Sidebar : React.FC<SideBarProps> = (props) => {
 
-    return (
-        <div className="sidebar">
-            <CategoryHeader title="Categories" />
-            <Categories feedConfigManager={props.feedConfigManager} />    
-        </div>
-    );
+    return <ApplicationContext.Consumer>
+                {
+                    ({language}) => (
+                        <div className="sidebar">
+                            <CategoryHeader title={language.categories} />
+                            <Categories feedConfigManager={props.feedConfigManager} />    
+                        </div>
+                    )
+                }
+            </ApplicationContext.Consumer> 
 
 };
 
