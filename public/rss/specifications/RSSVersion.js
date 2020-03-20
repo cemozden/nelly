84ae25a42432fb8f290afbca2a3bc89b82680c9b
Cem Özden <cemozden.tr@gmail.com>
2020-03-20 15:03:17 +0100

    Removing folder

--- public/rss/specifications/RSSVersion.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var RSSVersion;
-(function (RSSVersion) {
-    RSSVersion[RSSVersion["RSS_20"] = 0] = "RSS_20";
-})(RSSVersion = exports.RSSVersion || (exports.RSSVersion = {}));
-//# sourceMappingURL=RSSVersion.js.map
