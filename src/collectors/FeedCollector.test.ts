89b487c7592f6688c18823e4bcecad7e95d61dee
Cem Özden <cemozden.tr@gmail.com>
2020-03-23 21:17:49 +0100

    Fixing tests

--- src/collectors/FeedCollector.test.ts
+++ src/collectors/FeedCollector.test.ts
@@ -16,26 +16,26 @@ describe('FeedCollector', () => {
         it('should reject with an error if no internet connection is available', () => {  
             feedConfig.url = 'http://unavaiable_address.com';
             
-            return expect(collectFeed(feedConfig)).rejects
-                .toThrowError(new FeedFetchError(`Unable to fetch the feed. (${feedConfig.url}) Please check that the feed url is valid or an internet connection is available.`));
+            return expect(collectFeed(feedConfig, [])).rejects
+                .toThrowError(new FeedFetchError(`Unable to fetch the feed. (${feedConfig.url}) Please check that there is an stable internet connection available or the feed source URL "${feedConfig.url}" is valid.`));
         });
 
         it('should reject with an error if no internet connection is available', () => {  
             feedConfig.url = 'unavaiable_address.com';
             
-            return expect(collectFeed(feedConfig)).rejects.toThrow();
+            return expect(collectFeed(feedConfig, [])).rejects.toThrow();
         });
 
         it('should reject with an error if the response body is not an XML content', () => {
             feedConfig.url = 'https://github.com/cemozden/nelly/archive/master.zip';
             
-            return expect(collectFeed(feedConfig)).rejects.toThrow();
+            return expect(collectFeed(feedConfig, [])).rejects.toThrow();
         });
 
         it('should resolve with a valid value', () => {
             feedConfig.url = 'https://gizmodo.com/rss';
             
-            return expect(collectFeed(feedConfig)).resolves.not.toBeUndefined();
+            return expect(collectFeed(feedConfig, [])).resolves.not.toBeUndefined();
         });
 
     });
