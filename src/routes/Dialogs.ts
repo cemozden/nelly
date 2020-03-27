da88ef90402079222dfe80b6a473edeb17489e19
Cem Özden <cemozden.tr@gmail.com>
2020-03-27 16:35:53 +0100

    Delete feed UI implemented

--- src/routes/Dialogs.ts
+++ src/routes/Dialogs.ts
@@ -27,4 +27,24 @@ export default function Dialogs(exp : ExpressSettings, feedConfigManager : FeedC
         });
     });
 
+    exp.expressObject.get('/deletefeed_dialog', (req, res) => {
+        const params = req.query;
+        const feedId : string = params.feedId;
+
+        if (feedId === undefined || feedId.length === 0) {
+            const errorMessage =  'feedId parameter is missing!';
+            
+            res.status(400).json({ added : false, message : errorMessage });
+            http_logger.error(`[FeedContent] ${errorMessage}, Request params: ${JSON.stringify(params)}`);
+            
+            return;
+        }
+
+        const feedConfig = feedConfigManager.getFeedConfig(feedId);
+        console.log(feedConfig.fetchPeriod);
+        res.render('deletefeed', {
+            feedConfig
+        });
+    });
+
 }
